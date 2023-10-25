fb66f1d3c7e26c4687cc2c67ca49d12801eda4bb
patelsumit <patelsumit34@gmail.com>
2023-10-25 02:27:34 +0100

    Initial Commit

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -0,0 +1,10 @@
+package com.androiddevs.mvvmnewsapp.util
+
+class Constants {
+    companion object {
+        const val API_KEY = "d86dfca72ad14e1488b4d42ef5d906da"
+        const val BASE_URL = "https://newsapi.org"
+        const val SEARCH_DELAY = 500L
+        const val QUERY_PAGE_SIZE = 20
+    }
+}
